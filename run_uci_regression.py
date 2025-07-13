79e527cdccd6e3f86abedecabe335650af7f2154
dded1i <93054746+dded1i@users.noreply.github.com>
2025-07-13 17:56:28 +0200

    run_uci_regression.py type issue resolved
    added isinstance of block to def solve_reg method to resolve type vs dtype error

--- run_uci_regression.py
+++ run_uci_regression.py
@@ -13,19 +13,23 @@ from ntk_random_features import NtkFeatureMapOps
 
 def solve_reg(x_tr, y_tr, x_te, y_te, lam, y_std=1.0):
     n, d = x_tr.shape
-    xtype = x_tr.type()
+
+    # Ensure all inputs are torch.DoubleTensor regardless of source
+    x_tr = x_tr.double() if isinstance(x_tr, torch.Tensor) else torch.from_numpy(x_tr).double()
+    y_tr = y_tr.double() if isinstance(y_tr, torch.Tensor) else torch.from_numpy(y_tr).double()
+    x_te = x_te.double() if isinstance(x_te, torch.Tensor) else torch.from_numpy(x_te).double()
+    y_te = y_te.double() if isinstance(y_te, torch.Tensor) else torch.from_numpy(y_te).double()
 
     if x_tr.shape[0] > x_tr.shape[1]:
         b = x_tr.T @ (y_tr[:, None] if y_tr.dim() == 1 else y_tr)
-        y_pred = x_te @ torch.linalg.solve(x_tr.T @ x_tr + lam * torch.eye(d).type(xtype), b)
+        y_pred = x_te @ torch.linalg.solve(x_tr.T @ x_tr + lam * torch.eye(d, dtype=torch.float64), b)
     else:
         b = y_tr[:, None] if y_tr.dim() == 1 else y_tr
-        y_pred = x_te @ (x_tr.T @ torch.linalg.solve(x_tr @ x_tr.T + lam * torch.eye(n).type(xtype), b))
+        y_pred = x_te @ (x_tr.T @ torch.linalg.solve(x_tr @ x_tr.T + lam * torch.eye(n, dtype=torch.float64), b))
 
     mse = mean_squared_error((y_pred * y_std).cpu(), (y_te * y_std).cpu())
     acc = ((y_pred.argmax(axis=1) == y_te.argmax(axis=1)) * 1.0).mean().item()
     return mse, acc
-
 
 def hyperparams_search(LAMBDA_LIST, X, y, train_fold, valid_fold, y_std=1.0):
 
